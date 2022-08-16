d598b56bf42f222e80c91c3ef39c96718f9706bf
a1pro <teamlead@a1professionals.com>
2022-08-16 15:23:59 +0530

    Initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,61 @@
+<section id="bottom">
+    <div class="container wow fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="600ms" style="visibility: visible; animation-duration: 1000ms; animation-delay: 600ms; animation-name: fadeInDown;">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="widget">
+                    <h3>Company</h3>
+                    <ul>
+                        <li><a href="{{ url('') }}">Home</a></li>
+                        <li><a href="{{ url('') }}">About Us</a></li>
+                        <li><a href="{{ url('') }}">Contact Us</a></li>
+						<li><a href="{{ url('') }}">Thank You</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <div class="widget">
+                    <h3>Developers</h3>
+                    <ul>
+                        <li><a href="{{ url('') }}">Software Development</a></li>
+                        <li><a href="{{ url('') }}">Creative Writting</a></li>
+                        <li><a href="{{ url('') }}">UI Design</a></li>
+                        <li><a href="{{ url('') }}">Digital Marketing</a></li>
+                        <li><a href="{{ url('') }}">Mobile Responsive</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="widget">
+                    <h3>Our Partners</h3>
+                    <ul>
+                        <li><a href="#">Paradox</a></li>
+                        <li><a href="#">Split Rain</a></li>
+                        <li><a href="#">Theme Rectar</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="widget">
+                    <h3>Connect With Us</h3>
+                    <ul class="social-media">
+                        <li><a href="https://www.facebook.com/eweba1professionals" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/a1professionals" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="https://www.linkedin.com/pub/eweb-a1professionals-pvt-ltd/ab/230/628" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer id="footer" class="midnight-blue">
+    <div class="container">
+        <div class="row">
+            <center>
+                <div class="col-sm-12">
+                   <p> © 2020 eWeb A1Professionals - committed to quality. All Rights Reserved. </p>
+                </div>
+            </center>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.app')
+@section('content')
+<section class="hero-section text-center" id="home">
+    <div class="container green-box round-15">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="hero-wrapper mb-4">
+    			    <h3 class="white-text"><b>Make Your Booking Online<br> In Seconds...<br> <a href="" id="" class="" target="_parent" rel="noopener noreferrer" style="color: rgba(245, 173, 67, 0.96);">0800 0016 735</a>&nbsp;</b></h3>
+                    <img src="https://web.a1professionals.net/cleaner/storage/users/unnamed.png" class="mx-auto">
+                    <div class="slider-box mt-3 mb-3">
+    				   <h3 class="mb-4 white-text">Advanced Software <br>Your Local Oven Cleaning Company </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row pt-0">
+            <div class="col-lg-12 text-center bounce">
+                <a href="" class="btn btn-danger active btn-lg">Get Prices And Check Availability</a>
+            </div>
+        </div>
+        <div class="row pt-0 mt-4">
+            <div class="col-lg-12 px-0 mt-4">
+            <br>
+                 <h3 class="white-text"> <b>Simple Automated Booking System</b></h3>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section bg-blue text-center pb-2 pt-2" id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4">
+                <div class="text-center p-4 mt-3">
+                    <div class="avatar-md mx-auto mb-4">
+                         <span class="avatar-title services-icons">
+                            <i class="fa_prepended fa fa-diamond" contenteditable="false" style="color: rgb(255, 255, 255);"></i>
+                         </span>
+                    </div>
+                    <h4 class="white-text">Trained Professional Cleaners</h4>
+                    <p class="mb-0">All our local oven cleaners have been trained to the highest of standards and only use Eco-friendly products to remove the carbon, grease, and grime. Our aim is to bring your oven back to nearly new and that's a Advanced Software promise.</p>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4">
+                <div class="text-center p-4 mt-3">
+                    <div class="avatar-md mx-auto mb-4">
+                      <span class="avatar-title services-icons">
+                         <i class="fa_prepended fa fa-university" contenteditable="false" style="color: rgb(255, 255, 255);"></i>
+                      </span>
+                    </div>
+                    <h4 class="white-text">All Cleaners Are Fully Employed</h4>
+                    <p class="mb-0">Your oven cleaner will be from your local area, which means we can be flexible and clean your oven at a time that suits yourself. It takes seconds to book online. Select your oven, check your availability and we will bring your oven back to new.</p>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4">
+                <div class="text-center p-4 mt-3">
+                    <div class="avatar-md mx-auto mb-4">
+                      <span class="avatar-title services-icons">
+                        <i class="fa_prepended fa fa-hand-stop-o" contenteditable="false" style="color: rgb(255, 255, 255);"></i>
+                      </span>
+                    </div>
+                    <h4 class="white-text">How To Get Prices And Check Availability</h4><p class="mb-0">It couldn't be easier to book your professional oven clean. Click your oven type below to get an instant price, check the availability of your local oven cleaner and make your booking online in seconds.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section bg-light text-center pt-4 pb-5" id="benefits">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="text-center mb-5">
+                  <h3 class="blue-text"><b>What is a professional deep oven clean?</b></h3>
+                    <p class="big-text">We use no harmful chemicals to humans or the oven and leave no smells or odour</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+              <ul class="list-group text-left">
+                <li class="list-group-item">
+                  <div class="md-v-line"></div><i class="fa fa-check"></i> <span><b>Cover The Surround Area:</b> We cover the floor and surrounding area to ensure there is no additional mess in the kitchen. You can still use the kitchen while we are cleaning.</span>
+                </li>
+                <li class="list-group-item">
+                  <div class="md-v-line"></div><i class="fa fa-check"></i> <span><b>Remove Racks &amp; Shelves:</b> We remove all the racks, knobs and shelves, which will allow us full access to the oven, the racks and shelves are cleaned inside the dip tank to make them brand new.</span>
+                </li>
+                <li class="list-group-item">
+                  <div class="md-v-line"></div><i class="fa fa-check"></i> <span><b>Deep Clean The Oven:</b> We deep clean the oven using Eco-friendly products, getting into those hard to reach places.</span>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul class="list-group text-left">
+                <li class="list-group-item">
+                  <div class="md-v-line"></div><i class="fa fa-check"></i> <span><b>Clean The Racks &amp; Shelves:</b> We remove all the carbon, soot, grease and grime, by cleaning them all in our advanced dip tank system, then we place them back in position.</span>
+                </li>
+                <li class="list-group-item">
+                  <div class="md-v-line"></div><i class="fa fa-check"></i> <span><b>Remove &amp; Clean Oven Doors:</b> Carefully remove the oven doors to get rid of the built up grease and grime and ensure the seals are secure before reassembly.</span>
+                </li>
+                <li class="list-group-item">
+                  <div class="md-v-line"></div><i class="fa fa-check"></i> <span><b>Clean hobs/Hood:</b> We will professionally clean any hobs or extractor fans if requested and bring them back to new.</span>
+                </li>
+
+              </ul>
+
+            </div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-lg-12 text-center">
+               <button href="#" class="btn btn-primary btn-lg">Get Prices Online<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right icons-sm ml-1"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></button>
+            </div>
+        </div>  
+    </div>
+</section>
+@endsection
